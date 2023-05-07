5feb58382ece79ce7f81387d8738d41c92a771dd
Timerbulat <TimerbulatGilmutdinov@github.com>
2023-05-07 14:25:33 +0300

    Implemented AlreadyExistsException handling in @ControllerAdvice

--- src/main/java/ru/itis/easttrade/exceptions/AlreadyExistsException.java
+++ src/main/java/ru/itis/easttrade/exceptions/AlreadyExistsException.java
@@ -1,7 +1,16 @@
 package ru.itis.easttrade.exceptions;
 
 public class AlreadyExistsException extends RuntimeException {
+    private Object model;
     public AlreadyExistsException(String message) {
         super(message);
+    }
+    public AlreadyExistsException(String message, Object model){
+        super(message);
+        this.model = model;
+    }
+
+    public Object getModel() {
+        return model;
     }
 }
