52aa4ef3d8c3fb1d3e1b3d6e59d3f2ccfd7e592f
Timerbulat <TimerbulatGilmutdinov@github.com>
2023-05-13 14:31:02 +0300

    moved styles to separate files, added csrf tokens handling in forms and SecurityConfig

--- src/main/java/ru/itis/easttrade/config/ViewResolversConfig.java
+++ src/main/java/ru/itis/easttrade/config/ViewResolversConfig.java
@@ -1,4 +1,4 @@
-package ru.itis.easttrade.config.viewreolvers;
+package ru.itis.easttrade.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
