9dae73d07e566d84ec6f959a9a3454d5b4ebd943
Timerbulat <TimerbulatGilmutdinov@github.com>
2023-05-17 16:44:58 +0300

    Added ajax requests for deleting articles, added '/my-articles' view

--- src/main/java/ru/itis/easttrade/controllers/ArticlesController.java
+++ src/main/java/ru/itis/easttrade/controllers/ArticlesController.java
@@ -5,9 +5,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
+import ru.itis.easttrade.dto.AccountDto;
 import ru.itis.easttrade.dto.ArticleDto;
 import ru.itis.easttrade.dto.NewOrUpdateArticleDto;
+import ru.itis.easttrade.dto.TaskDto;
+import ru.itis.easttrade.services.AccountsService;
 import ru.itis.easttrade.services.ArticlesService;
+import ru.itis.easttrade.services.TasksService;
 
 import java.security.Principal;
 import java.util.List;
@@ -15,6 +19,8 @@ import java.util.List;
 @Controller
 @RequiredArgsConstructor
 public class ArticlesController {
+    private final TasksService tasksService;
+    private final AccountsService accountsService;
     private final ArticlesService articlesService;
 
     @GetMapping("/articles")
@@ -31,6 +37,13 @@ public class ArticlesController {
         return "articles";
     }
 
+    @GetMapping("/my-articles")
+    public String myArticles(Model model, Principal principal) {
+        AccountDto accountDto = accountsService.getAccountByEmail(principal.getName());
+        List<ArticleDto> articles = articlesService.getArticlesByAccount(accountDto);
+        model.addAttribute("articles", articles);
+        return "my-articles";
+    }
     @GetMapping("/articles/{id}")
     public String getArticleById(@PathVariable("id") Integer id, Model model) {
         model.addAttribute("article", articlesService.getArticleById(id));
@@ -61,4 +74,8 @@ public class ArticlesController {
         return "redirect:"+MvcUriComponentsBuilder.fromMappingName("AC#getArticleById").arg(0,id).build();
     }
 
+    @DeleteMapping("/articles/{id}")
+    public void deleteArticle(@PathVariable("id") Integer id){
+        articlesService.deleteArticleById(id);
+    }
 }
