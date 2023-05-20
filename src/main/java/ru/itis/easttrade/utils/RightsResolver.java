38200102801c4977c402664e8a6793320d656825
Timerbulat <timerhelmut@gmail.com>
2023-05-20 16:58:59 +0300

    Fixed error when not authenticated user tries to update article or task

--- src/main/java/ru/itis/easttrade/utils/RightsResolver.java
+++ src/main/java/ru/itis/easttrade/utils/RightsResolver.java
@@ -5,8 +5,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 import ru.itis.easttrade.repositories.ArticlesRepository;
 import ru.itis.easttrade.repositories.TasksRepository;
-import ru.itis.easttrade.services.ArticlesService;
-import ru.itis.easttrade.services.TasksService;
 
 @Component
 @RequiredArgsConstructor
@@ -15,11 +13,17 @@ public class RightsResolver {
     private final TasksRepository tasksRepository;
     private final ArticlesRepository articlesRepository;
     public boolean resolveTaskAction(Integer taskId, Authentication authentication){
+        if(authentication ==null){
+            return false;
+        }
         boolean isAuthor = tasksRepository.findById(taskId).get().getAccount().getEmail().equals(authentication.getName());
         return isAuthor||roleChecker.hasEnoughAuthority(authentication);
     }
 
     public boolean resolveArticleAction(Integer articleId, Authentication authentication){
+        if(authentication == null){
+            return false;
+        }
         boolean isAuthor = articlesRepository.findById(articleId).get().getAccount().getEmail().equals(authentication.getName());
         return isAuthor||roleChecker.hasEnoughAuthority(authentication);
     }
