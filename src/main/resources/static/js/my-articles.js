40fd104e41185b1fd179fde566f4a785d191c3fb
Timerbulat <timerhelmut@gmail.com>
2023-05-21 12:55:52 +0300

    added handling csrf token in AJAX requests

--- src/main/resources/static/js/my-articles.js
+++ src/main/resources/static/js/my-articles.js
@@ -1,8 +1,13 @@
 function deleteArticle(taskId) {
+    let token = $("meta[name='_csrf']").attr("content");
+    let header = $("meta[name='_csrf_header']").attr("content");
     if (confirm("Are you sure you want to delete this article?")) {
         $.ajax({
             url: "/articles/" + taskId,
             method: "DELETE",
+            headers:{
+                [header]:token
+            },
             success: function (result) {
                 location.reload();
             }
--- src/main/resources/static/js/my-tasks.js
+++ src/main/resources/static/js/my-tasks.js
@@ -1,8 +1,13 @@
 function deleteTask(taskId) {
+    let token = $("meta[name='_csrf']").attr("content");
+    let header = $("meta[name='_csrf_header']").attr("content");
     if (confirm("Are you sure you want to delete this task?")) {
         $.ajax({
             url: "/tasks/" + taskId,
             method: "DELETE",
+            headers:{
+                [header]:token
+            },
             success: function () {
                 location.reload();
             }
