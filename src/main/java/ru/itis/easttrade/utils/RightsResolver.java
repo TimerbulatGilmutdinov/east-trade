3b8ea5c450ff4c1bbe6018d80bb6cb3fd686aed3
Timerbulat <timerhelmut@gmail.com>
2023-05-21 17:33:34 +0300

    fixed bug when refreshing '/exampleEntity/{id}' page after deleting one fails with 500 error

--- src/main/java/ru/itis/easttrade/utils/RightsResolver.java
+++ src/main/java/ru/itis/easttrade/utils/RightsResolver.java
@@ -3,6 +3,7 @@ package ru.itis.easttrade.utils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+import ru.itis.easttrade.exceptions.NotFoundException;
 import ru.itis.easttrade.repositories.ArticlesRepository;
 import ru.itis.easttrade.repositories.ProductsRepository;
 import ru.itis.easttrade.repositories.TasksRepository;
@@ -14,27 +15,28 @@ public class RightsResolver {
     private final TasksRepository tasksRepository;
     private final ArticlesRepository articlesRepository;
     private final ProductsRepository productsRepository;
-    public boolean resolveTaskAction(Integer taskId, Authentication authentication){
-        if(authentication ==null){
+
+    public boolean resolveTaskAction(Integer taskId, Authentication authentication) {
+        if (authentication == null) {
             return false;
         }
-        boolean isAuthor = tasksRepository.findById(taskId).get().getAccount().getEmail().equals(authentication.getName());
-        return isAuthor||roleChecker.hasEnoughAuthority(authentication);
+        boolean isAuthor = tasksRepository.findById(taskId).orElseThrow(() -> new NotFoundException("Task with id <" + taskId + "> not found")).getAccount().getEmail().equals(authentication.getName());
+        return isAuthor || roleChecker.hasEnoughAuthority(authentication);
     }
 
-    public boolean resolveArticleAction(Integer articleId, Authentication authentication){
-        if(authentication == null){
+    public boolean resolveArticleAction(Integer articleId, Authentication authentication) {
+        if (authentication == null) {
             return false;
         }
-        boolean isAuthor = articlesRepository.findById(articleId).get().getAccount().getEmail().equals(authentication.getName());
-        return isAuthor||roleChecker.hasEnoughAuthority(authentication);
+        boolean isAuthor = articlesRepository.findById(articleId).orElseThrow(() -> new NotFoundException("Article with id <" + articleId + "> not found")).getAccount().getEmail().equals(authentication.getName());
+        return isAuthor || roleChecker.hasEnoughAuthority(authentication);
     }
 
-    public boolean resolveProductAction(Integer id, Authentication authentication){
-        if(authentication == null){
+    public boolean resolveProductAction(Integer id, Authentication authentication) {
+        if (authentication == null) {
             return false;
         }
-        boolean isAuthor = productsRepository.findById(id).get().getAccount().getEmail().equals(authentication.getName());
-        return isAuthor||roleChecker.hasEnoughAuthority(authentication);
+        boolean isAuthor = productsRepository.findById(id).orElseThrow(() -> new NotFoundException("Product with id <" + id + "> not found")).getAccount().getEmail().equals(authentication.getName());
+        return isAuthor || roleChecker.hasEnoughAuthority(authentication);
     }
 }
