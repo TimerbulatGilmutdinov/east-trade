4fee6f2bd0196fb440a750dc6c054bc407944cf9
Timerbulat <timerhelmut@gmail.com>
2023-05-25 09:02:42 +0300

    Added logout controller, because Spring Security's doesn't work

--- src/main/java/ru/itis/easttrade/controllers/WelcomeController.java
+++ src/main/java/ru/itis/easttrade/controllers/WelcomeController.java
@@ -1,11 +1,16 @@
 package ru.itis.easttrade.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import ru.itis.easttrade.repositories.AccountsRepository;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 @Controller
 @RequiredArgsConstructor
@@ -18,5 +23,12 @@ public class WelcomeController {
     @GetMapping("/")
     public String redirectToWelcomePage(){
         return "redirect:"+ MvcUriComponentsBuilder.fromMappingName("WC#getWelcomePage").build();
+    }
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication){
+        if(authentication!=null){
+            new SecurityContextLogoutHandler().logout(request,response,authentication);
+        }
+        return "redirect:"+"/welcome";
     }
 }
